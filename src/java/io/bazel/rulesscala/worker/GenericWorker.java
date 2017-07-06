20bd2558c6a85e5fce406fbfbb7798ec8401c28f
Jeff Balogh <jeff@jbalogh.me>
2017-07-05 19:30:31 -0700

    Update GenericWorker.java

--- src/java/io/bazel/rulesscala/worker/GenericWorker.java
+++ src/java/io/bazel/rulesscala/worker/GenericWorker.java
@@ -19,6 +19,126 @@ import java.util.Map;
 import java.util.TreeMap;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FilteredOutputStream;
+
+public class TeeInputStream extends FilteredOutputStream {
+
+    /**
+     * The output stream that will receive a copy of all bytes read from the
+     * proxied input stream.
+     */
+    private final OutputStream branch;
+
+    /**
+     * Flag for closing also the associated output stream when this
+     * stream is closed.
+     */
+    private final boolean closeBranch;
+
+    /**
+     * Creates a TeeInputStream that proxies the given {@link InputStream}
+     * and copies all read bytes to the given {@link OutputStream}. The given
+     * output stream will not be closed when this stream gets closed.
+     *
+     * @param input input stream to be proxied
+     * @param branch output stream that will receive a copy of all bytes read
+     */
+    public TeeInputStream(final InputStream input, final OutputStream branch) {
+        this(input, branch, false);
+    }
+
+    /**
+     * Creates a TeeInputStream that proxies the given {@link InputStream}
+     * and copies all read bytes to the given {@link OutputStream}. The given
+     * output stream will be closed when this stream gets closed if the
+     * closeBranch parameter is {@code true}.
+     *
+     * @param input input stream to be proxied
+     * @param branch output stream that will receive a copy of all bytes read
+     * @param closeBranch flag for closing also the output stream when this
+     *                    stream is closed
+     */
+    public TeeInputStream(
+            final InputStream input, final OutputStream branch, final boolean closeBranch) {
+        super(input);
+        this.branch = branch;
+        this.closeBranch = closeBranch;
+    }
+
+    /**
+     * Closes the proxied input stream and, if so configured, the associated
+     * output stream. An exception thrown from one stream will not prevent
+     * closing of the other stream.
+     *
+     * @throws IOException if either of the streams could not be closed
+     */
+    @Override
+    public void close() throws IOException {
+        try {
+            super.close();
+        } finally {
+            if (closeBranch) {
+                branch.close();
+            }
+        }
+    }
+
+    /**
+     * Reads a single byte from the proxied input stream and writes it to
+     * the associated output stream.
+     *
+     * @return next byte from the stream, or -1 if the stream has ended
+     * @throws IOException if the stream could not be read (or written) 
+     */
+    @Override
+    public int read() throws IOException {
+        final int ch = super.read();
+        if (ch != -1) {
+            branch.write(ch);
+        }
+        return ch;
+    }
+
+    /**
+     * Reads bytes from the proxied input stream and writes the read bytes
+     * to the associated output stream.
+     *
+     * @param bts byte buffer
+     * @param st start offset within the buffer
+     * @param end maximum number of bytes to read
+     * @return number of bytes read, or -1 if the stream has ended
+     * @throws IOException if the stream could not be read (or written) 
+     */
+    @Override
+    public int read(final byte[] bts, final int st, final int end) throws IOException {
+        final int n = super.read(bts, st, end);
+        if (n != -1) {
+            branch.write(bts, st, n);
+        }
+        return n;
+    }
+
+    /**
+     * Reads bytes from the proxied input stream and writes the read bytes
+     * to the associated output stream.
+     *
+     * @param bts byte buffer
+     * @return number of bytes read, or -1 if the stream has ended
+     * @throws IOException if the stream could not be read (or written) 
+     */
+    @Override
+    public int read(final byte[] bts) throws IOException {
+        final int n = super.read(bts);
+        if (n != -1) {
+            branch.write(bts, 0, n);
+        }
+        return n;
+    }
+
+}
+
 public class GenericWorker {
   final protected Processor processor;
 
@@ -35,10 +155,15 @@ public class GenericWorker {
   private void runPersistentWorker() throws IOException {
     PrintStream originalStdOut = System.out;
     PrintStream originalStdErr = System.err;
+    
+    
 
     while (true) {
       try {
-        WorkRequest request = WorkRequest.parseDelimitedFrom(System.in);
+        
+        
+        
+        WorkRequest request = WorkRequest.parseDelimitedFrom(new TeeInputStream(System.in, System.out));
         if (request == null) {
           break;
         }
